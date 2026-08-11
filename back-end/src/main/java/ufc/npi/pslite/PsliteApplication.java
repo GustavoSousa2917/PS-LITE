8015afa5f46a741f9a4448893952bc11685e6318
gustavo <gustavo.sousa2917@gmail.com>
2026-08-11 12:03:55 -0300

    chore: initial commit com estrutura base, backend e frontend

---
+++ back-end/src/main/java/ufc/npi/pslite/PsliteApplication.java
@@ -0,0 +1,13 @@
+package ufc.npi.pslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsliteApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/ufc/npi/pslite/PsliteApplicationTests.java
@@ -0,0 +1,13 @@
+package ufc.npi.pslite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
