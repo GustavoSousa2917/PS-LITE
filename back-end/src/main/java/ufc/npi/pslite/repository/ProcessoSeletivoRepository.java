16a608247eccf8a5f1befb9f5900b706a707643d
gustavo <gustavo.sousa2917@gmail.com>
2026-08-12 10:46:43 -0300

    feat: cria entidade ProcessoSeletivo, repository e migration V1 do Flyway

---
+++ back-end/src/main/java/ufc/npi/pslite/repository/ProcessoSeletivoRepository.java
@@ -0,0 +1,8 @@
+package ufc.npi.pslite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ufc.npi.pslite.model.ProcessoSeletivo;
+
+public interface ProcessoSeletivoRepository extends JpaRepository<ProcessoSeletivo, Long> {
+
+}
---
+++ back-end/src/main/resources/db/migration/V1__processo_seletivo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE processo_seletivo (
+                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                   nome VARCHAR(255) NOT NULL,
+                                   descricao VARCHAR(255),
+                                   qtd_vagas INT DEFAULT 0 CHECK (qtd_vagas >= 0),
+                                   status VARCHAR(50) DEFAULT 'CADASTRADO'
+);
