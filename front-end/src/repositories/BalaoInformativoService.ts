e4742b952c9a5ce738209b520dbf9481ccff8754
Rhian Martins <castrorhiancastro@gmail.com>
2026-08-18 11:45:01 -0300

    feat: service de baloes agora  utilizando o HttpClient real

--- front-end/src/repositories/BalaoInformativoService.ts
+++ front-end/src/repositories/BalaoInformativoService.ts
@@ -1,21 +1,27 @@
-import {BalaoInformativo} from "../types/BalaoInformativo";
+import type { BalaoInformativo } from '../types/BalaoInformativo';
+import HttpClient from './adapters/HttpClient';
 
 export const BalaoInformativoService = {
-    async listByProcesso(idProcesso: number) {
-        console.log(`simulando busca de balões para o processo ${idProcesso}`);
-        //Futuramente será algo como: return httpClient.get(`/balao-informativo/por-processo/${idProcesso}`)
-        return [];
+
+    async listByProcesso(idProcesso: number): Promise<BalaoInformativo[]> {
+        const response = await HttpClient.get(`/balao-informativo/por-processo/${idProcesso}`);
+        return response.data;
     },
-    async getById(idBalao: number) {
-        console.log(`Simulando busca de balão ${idBalao}`);
+
+    async getById(idBalao: number): Promise<BalaoInformativo> {
+        const response = await HttpClient.get(`/balao-informativo/${idBalao}`);
+        return response.data;
     },
-    async create(balao: BalaoInformativo) {
-        console.log('Simulando criação do balão:', balao);
+
+    async create(balao: BalaoInformativo): Promise<void> {
+        await HttpClient.post('/balao-informativo', balao);
     },
-    async update(idBalao: number, balao: BalaoInformativo) {
-        console.log(`Simulando atualização do balão ${idBalao}:`, balao);
+
+    async update(idBalao: number, balao: BalaoInformativo): Promise<void> {
+        await HttpClient.put(`/balao-informativo/${idBalao}`, balao);
     },
-    async delete(idBalao: number) {
-        console.log(`Simulando deleção do balão ${idBalao}`);
+
+    async delete(idBalao: number): Promise<void> {
+        await HttpClient.delete(`/balao-informativo/${idBalao}`);
     }
 };
