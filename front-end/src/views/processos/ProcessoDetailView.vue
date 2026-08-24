d5728065d6557f8301778d757988e03d4e937005
Rhian Martins <castrorhiancastro@gmail.com>
2026-08-24 11:10:20 -0300

    fix: corrige case sensitive do css do appbar e testa componente

--- front-end/src/views/processos/ProcessoDetailView.vue
+++ front-end/src/views/processos/ProcessoDetailView.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import BaloesDoProcesso from '../../components/BaloesDoProcesso.vue';
+
+const route = useRoute();
+</script>
+
 <template>
-    <h1>Detalhes do Processo</h1>
+  <div class="pa-4">
+    <h1>Detalhes do Processo (Em construção)</h1>
+
+    <v-divider class="my-4"></v-divider>
+
+    <BaloesDoProcesso :processoId="Number(route.params.id)" />
+  </div>
 </template>
